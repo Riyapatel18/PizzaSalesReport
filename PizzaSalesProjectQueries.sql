141aebad1c4f86d06a9b6d728e9560a665f8c786
Riya Patel <83216030+Riyapatel18@users.noreply.github.com>
2026-03-31 11:15:19 +0530

    Pizza sales analysis Power BI report

---
+++ PizzaSalesProjectQueries.sql
@@ -0,0 +1,88 @@
+SELECT * FROM pizza_sales
+
+SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
+
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza_sales
+
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales
+
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales
+
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza_sales
+
+SELECT DAYNAME(STR_TO_DATE(order_date, '%d-%m-%Y')) AS order_day,
+       COUNT(DISTINCT order_id) AS total_orders
+FROM pizza_sales
+GROUP BY DAYNAME(STR_TO_DATE(order_date, '%d-%m-%Y'));
+
+SELECT MONTHNAME(STR_TO_DATE(order_date,'%d-%m-%Y')) AS Month_Name,
+       COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY MONTH(STR_TO_DATE(order_date,'%d-%m-%Y')),
+         MONTHNAME(STR_TO_DATE(order_date,'%d-%m-%Y'))
+ORDER BY MONTH(STR_TO_DATE(order_date,'%d-%m-%Y'));
+
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category
+
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_size
+ORDER BY pizza_size
+
+
+SELECT pizza_category,
+       SUM(quantity) AS Total_Quantity_Sold
+FROM pizza_sales
+WHERE MONTH(STR_TO_DATE(order_date,'%d-%m-%Y')) = 2
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC;
+
+SELECT pizza_name,
+       SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue DESC
+LIMIT 5;
+
+SELECT pizza_name,
+       SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue ASC
+LIMIT 5;
+
+SELECT pizza_name,
+       SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+LIMIT 5;
+
+SELECT pizza_name,
+       SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold ASC
+LIMIT 5;
+
+SELECT pizza_name,
+       COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders DESC
+LIMIT 5;
+
+SELECT pizza_name,
+       COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders ASC
+LIMIT 5;
+
